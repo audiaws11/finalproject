d938d92d025b826efb1ff8fb8873a4e4635093ae
Audia Winniar Savitri <audiaws@Gians-MacBook-Air.local>
2024-04-25 12:16:28 -0500

    update 27/4: edit 1

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: './', // Set the base path if needed, especially when deploying to a subdirectory
+  build: {
+    outDir: 'dist', // Default output directory, make sure this matches what Netlify expects
+  }
+});
